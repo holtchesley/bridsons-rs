c388d30bfba1e1da233eff659e49240eb36c63db
Brennan Chesley <holtchesley@example.com>
2016-04-12 08:43:52 -0400

    Implementation of Bridson's Algorithm working for 2d.
    Future work might involve abstracting dimensionality out so that the alg works for Rn.

---
+++ src/lib.rs
@@ -0,0 +1,179 @@
+#![feature(test)]
+
+
+extern crate rand;
+extern crate test;
+
+
+pub mod bridsons {
+
+    use rand::Rng;
+    use std::f32;
+    use std::collections::HashMap;
+    pub const SQRT2: f32 = 1.414213562373095048;
+
+    #[derive(Clone, Debug)]
+    pub struct Point2d {
+        x: f32,
+        y: f32
+    }
+
+    impl Point2d {
+
+        pub fn rand_in_rect(width:f32, height:f32, seed: &mut Rng) -> Point2d {
+            Point2d{x: seed.next_f32()* width, y: seed.next_f32()*height}
+        }
+
+        pub fn d(&self,other: &Point2d) -> f32{
+            let xd = other.x - self.x;
+            let yd = other.y - self.y;
+            return xd.hypot(yd);
+        }
+
+        pub fn rand_in_annulus(&self, r: f32, rand: &mut Rng) -> Point2d {
+            let radius = rand.next_f32().mul_add(r,r);
+            let (xd,yd) = (rand.next_f32() * f32::consts::PI).sin_cos();
+            return Point2d{x:xd*radius+ self.x, y:yd*radius+self.y}
+        }
+
+        pub fn in_box(&self, other: &Point2d) -> bool {
+            return (self.x < other.x) && (self.y < other.y) && (self.x > 0.0) && (self.y > 0.0)
+        }
+
+        pub fn within_r(&self, r: f32, other: &Point2d) -> bool {
+            return self.d(other) < r;
+        }
+
+    }
+
+
+    pub struct Grid2d {
+        grid_size: f32,
+        width: u32,
+        height: u32,
+        grid: HashMap<(u32,u32),Point2d>
+    }
+
+    impl Grid2d {
+        pub fn new() -> Grid2d {
+            Grid2d{grid_size: 0.0, width: 0, height: 0, grid: HashMap::new()}
+        }
+
+        pub fn init_rect(&mut self, width:f32, height:f32, radius:f32) {
+            self.grid_size = radius / SQRT2;
+            self.width = (width/self.grid_size).floor() as u32;
+            self.height = (height/self.grid_size).floor() as u32;
+        }
+
+        fn grid_coord(&self, point: &Point2d) -> (u32, u32){
+            return ((point.x / self.grid_size).floor() as u32,(point.y / self.grid_size).floor() as u32)
+        }
+
+        pub fn has_nearby(&self, r: f32, point: &Point2d) -> bool {
+            let (cx,cy) = self.grid_coord(point);
+            fn buf_dec(a:u32) -> u32 {
+                if a > 1 {a-1} else {0}
+            }
+            for xx in buf_dec(cx)..(cx+2) {
+                for yy in buf_dec(cy)..(cy+2) {
+                    if let Some(pt) = self.grid.get(&(xx,yy)) {
+                        if pt.within_r(r, point) {
+                            return true
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+
+        pub fn add_point(&mut self, point: Point2d) {
+            let gc = self.grid_coord(&point);
+            self.grid.insert(gc,point);
+        }
+    }
+
+    pub fn bridsons_method_2d(width: f32, height: f32, radius: f32, attempts:u32, seed: &mut Rng) -> Vec<Point2d> {
+        let mut active_set = vec![];
+        let mut inactive_set = vec![];
+        let mut grid = Grid2d::new();
+        let ur_corner = Point2d{x:width, y:height};
+        grid.init_rect(width, height, radius);
+        let pt = Point2d::rand_in_rect(width, height, seed);
+        active_set.push(pt.to_owned());
+        grid.add_point(pt);
+
+        while !active_set.is_empty() {
+            let redundant_len_because_rust_is_silly = (seed.next_u32() as usize) % active_set.len();
+            let active_point = active_set.swap_remove(redundant_len_because_rust_is_silly);
+            if let Some(found_pt) = (0..attempts).map(|_| active_point.rand_in_annulus(radius, seed)).find(|pt| pt.in_box(&ur_corner) && !grid.has_nearby(radius,pt)) {
+                active_set.push(active_point);
+                active_set.push(found_pt.to_owned());
+                grid.add_point(found_pt);
+            } else {
+                inactive_set.push(active_point);
+            }
+
+        }
+
+        return inactive_set;
+    }
+
+    #[cfg(test)]
+    mod tests {
+        extern crate rand;
+
+        use rand::{XorShiftRng, SeedableRng};
+        use super::bridsons_method_2d;
+        use test::Bencher;
+
+        #[test]
+        fn it_works() {
+            bridsons_method_2d(10.0, 10.0, 2.0, 30, &mut XorShiftRng::from_seed([4, 6, 2, 3]));
+        }
+
+        #[test]
+        fn it_works_count_10() {
+            println!("Count: {}", bridsons_method_2d(10.0, 10.0, 2.0, 30, &mut XorShiftRng::from_seed([4, 6, 2, 3])).len());
+        }
+
+        #[test]
+        fn it_works_count_100() {
+            println!("Count: {}", bridsons_method_2d(100.0, 100.0, 2.0, 30, &mut XorShiftRng::from_seed([4, 61234, 34342342, 4444443])).len());
+        }
+
+        #[test]
+        fn it_works_count_512() {
+            println!("Count: {}", bridsons_method_2d(512.0, 512.0, 2.0, 30, &mut XorShiftRng::from_seed([4, 6, 2, 3])).len());
+        }
+
+
+        #[bench]
+        fn bench_bridsons_008(b: &mut Bencher) {
+            b.iter(|| bridsons_method_2d(8.0, 8.0, 2.0, 30, &mut XorShiftRng::from_seed([4, 6, 2, 3])));
+        }
+
+        #[bench]
+        fn bench_bridsons_016(b: &mut Bencher) {
+            b.iter(|| bridsons_method_2d(16.0, 16.0, 2.0, 30, &mut XorShiftRng::from_seed([4, 6, 2, 3])));
+        }
+
+        #[bench]
+        fn bench_bridsons_032(b: &mut Bencher) {
+            b.iter(|| bridsons_method_2d(32.0, 32.0, 2.0, 30, &mut XorShiftRng::from_seed([4, 6, 2, 3])));
+        }
+
+        #[bench]
+        fn bench_bridsons_064(b: &mut Bencher) {
+            b.iter(|| bridsons_method_2d(64.0, 64.0, 2.0, 30, &mut XorShiftRng::from_seed([4, 6, 2, 3])));
+        }
+
+        #[bench]
+        fn bench_bridsons_128(b: &mut Bencher) {
+            b.iter(|| bridsons_method_2d(128.0, 128.0, 2.0, 30, &mut XorShiftRng::from_seed([4, 6, 2, 3])));
+        }
+
+    }
+}
+
+
+
